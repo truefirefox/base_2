b6455e2c23faf863226dcbaee7c6b6a18fd74c32
Tanechka <truefirefox@gmail.com>
2019-01-31 18:05:32 -0800

    hw_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,28 +1,46 @@
+import java.util.Arrays;
+
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
-    void save(Resume resume) {
+    void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
     }
 
-    void clear() {
-    }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
